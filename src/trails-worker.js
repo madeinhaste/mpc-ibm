415cbb0fc205a409e0d9c9cb03d9f76d7b83ed85
Daniel Lewis <actiondan@gmail.com>
2019-03-06 11:51:41 +0000

    cleanup

--- src/trails-worker.js
+++ src/trails-worker.js
@@ -7,9 +7,16 @@ console.log('hello from trails-worker');
 self.onmessage = function(e) {
     const cps = e.data.cps;
     const idxs = e.data.idxs;
+    for (let i = 0; i < idxs.length; ++i) {
+        const data = generate_trail(idxs[i], cps);
+        postMessage(data, [data.P]);
+    }
+};
 
-    const C = vec3.create();
+const C = vec3.create();
+const P = vec3.create();
 
+function generate_trail(idx, cps) {
     {
         // end point
         const sp = cps.length - 3;
@@ -18,35 +25,33 @@ self.onmessage = function(e) {
         C[2] = cps[sp + 2];
     }
 
-    const P = vec3.create();
 
-    for (let i = 0; i < idxs.length; ++i) {
-        const idx = idxs[i];
-        const len = ~~lerp(50, max_length, Math.random());
-        const out = new Float32Array(vertex_stride * len);
+    const len = ~~lerp(50, max_length, Math.random());
+    const out = new Float32Array(vertex_stride * len);
 
-        {
-            vec3.copy(P, C);
-            P[0] += random_gaussian(0, 50);
-            P[1] += random_gaussian(0, 50);
-            P[2] += random_gaussian(0, 50);
+    {
+        vec3.copy(P, C);
+        P[0] += random_gaussian(0, 50);
+        P[1] += random_gaussian(0, 50);
+        P[2] += random_gaussian(0, 50);
 
-            let dp = 0;
-            for (let j = 0; j < len; ++j) {
-                out[dp + 0] = P[0];
-                out[dp + 1] = P[1];
-                out[dp + 2] = P[2];
-                out[dp + 3] = j / (len - 1);
-                dp += 4;
+        let dp = 0;
+        for (let j = 0; j < len; ++j) {
+            out[dp + 0] = P[0];
+            out[dp + 1] = P[1];
+            out[dp + 2] = P[2];
+            out[dp + 3] = j / (len - 1);
+            dp += 4;
 
-                // advect
-                P[0] += random_gaussian(0, 1);
-                P[1] += random_gaussian(0, 1);
-                P[2] += random_gaussian(1, 2);
-            }
+            // advect
+            P[0] += random_gaussian(0, 1);
+            P[1] += random_gaussian(0, 1);
+            P[2] += random_gaussian(1, 2);
         }
-
-        const data = { idx, P: out.buffer };
-        postMessage(data, [data.P]);
     }
-};
+
+    return {
+        idx,
+        P: out.buffer,
+    };
+}
