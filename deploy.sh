e8a50188cf450b87b27c3cf983a5274a8ceb7ce1
Daniel Lewis <actiondan@gmail.com>
2019-03-07 12:44:09 +0000

    add sounds to deploy

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,13 @@ cp -a public/airplane.html dist/index.html
 mkdir dist/data
 mkdir dist/bundles
 mkdir dist/images
+mkdir dist/sounds
 
 cp -a public/bundles/airplane.bundle.js dist/bundles/
 cp -a public/bundles/trails-worker.bundle.js dist/bundles/
 cp -a public/images/{cloud10.png,cockpit.png,sky256.png,sky3.jpg} dist/images/
 cp -a public/data/* dist/data/
+cp -a public/sounds/* dist/sounds/
 
 #rsync -avL public/* dist/
 archive=dist-`date +%y%m%d`.zip
